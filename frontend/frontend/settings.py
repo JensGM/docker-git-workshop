098afbf9d03d155d20baca5a8fffa5324bb1110d
Jens Gåsemyr Magnus <jegm@equinor.com>
2021-02-12 09:28:58 +0100

    RC

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -78,18 +78,18 @@ WSGI_APPLICATION = 'frontend.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django',
-        'USER': 'postgres',
-        # 'PASSWORD': 'mypassword',
-        'HOST': 'postgres',
-        # 'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'django',
+    #     'USER': 'postgres',
+    #     # 'PASSWORD': 'mypassword',
+    #     'HOST': 'postgres',
+    #     # 'PORT': '5432',
+    # }
 }
 
 
